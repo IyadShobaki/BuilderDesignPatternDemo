2b2de8f4433774a612615dfac4afd4899feae38f
IyadShobaki <iyadshobaki82@gmail.com>
2021-01-26 13:51:14 -0500

    Initial commit

---
+++ BlazorUI/Data/CounterService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Data
+{
+    public class CounterService : ICounterService
+    {
+        private readonly ILogger<CounterService> _logger;
+
+        public int CounterValue { get; private set; }
+
+        public CounterService(ILogger<CounterService> logger)
+        {
+            _logger = logger;
+        }
+        public void IncreamentCounter()
+        {
+            CounterValue += 1;
+            _logger.LogInformation("The counter was incremented to {CounterValue}", CounterValue);
+        }
+    }
+}
---
+++ BlazorUI/Models/HtmlBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Models
+{
+    public class HtmlBuilder
+    {
+        private readonly string _rootName;
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName, string rootText = " ")
+        {
+            _rootName = rootName;
+            root.Name = rootName;
+            root.Text = rootText;
+        }
+        //public void AddChild(string childName,string childText)
+        public HtmlBuilder AddChild(string childName,string childText) // using fluent interface
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = _rootName };
+        }
+    }
+}
